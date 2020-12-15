8d134e6334183fe8a3e7a8731aa3b9590928113f
trisjoh <74061093+trisjoh@users.noreply.github.com>
2020-12-15 14:42:09 +0000

    Solution to Day 1

--- solutions/day01/sol01.py
+++ solutions/day01/sol01.py
@@ -1,45 +1,26 @@
-'''
-Problem 01
-
---- Day 1: Report Repair ---
-
-After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
-
-The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
-
-To save your vacation, you need to get all fifty stars by December 25th.
-
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-
-Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
-
-Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
-
-For example, suppose your expense report contained the following:
-
-1721
-979
-366
-299
-675
-1456
-
-In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
-
-Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
-
-
-'''
 def getSol():
     initialProblemInput = open("input01", "r").read().split("\n")
     del initialProblemInput[len(initialProblemInput)-1]
     problemInput = []
 
-    for val in initialproblemInput:
+    for val in initialProblemInput:
         problemInput.append(int(val))
+
+    solutions = {}
 
     for val1 in problemInput:
         for val2 in problemInput[problemInput.index(val1)+1:]:
             if val1+val2 == 2020:
-                return val1*val2
-print(getSol())
+                solutions["one"] = val1*val2
+            else:
+                for val3 in problemInput[problemInput.index(val2)+1:]:
+                    if val1+val2+val3 == 2020:
+                        solutions["two"] = val1*val2*val3
+            if len(solutions) == 2:
+                return solutions
+
+
+solutions = getSol()
+
+print("Solution 1 is {}, and Solution 2 is {}.".format(solutions["one"], solutions["two"]))
+
